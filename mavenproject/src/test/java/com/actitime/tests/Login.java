fac7120f3015530a2b2ea749bb9883dda28ef199
tutorpatil <tutorpatil@gmail.com>
2022-05-25 07:42:28 +0530

    Pushing to remote

---
+++ mavenproject/src/test/java/com/actitime/tests/Login.java
@@ -0,0 +1,51 @@
+package com.actitime.tests;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v100.page.Page.CaptureScreenshotFormat;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.actitime.base.BaseClass;
+import com.actitime.utils.CommonUtils;
+
+public class Login extends BaseClass{
+	
+		
+	@Test
+	public static void login_001() throws Exception
+	{
+		boolean result = false;			
+	   		
+		writeInfoLogs("Trying to login to the actitime application by calling login method..");		
+		result = CommonUtils.loginToActiTimeApplication();		
+		
+		// Checking the result to be equal to true
+		Assert.assertTrue(result, "Could not login to ActiTime application..");
+			
+			
+		
+	}
+	
+	@Test
+	public static void login_002() throws Exception
+	{
+		boolean result = false;			
+   		
+		writeInfoLogs("Trying to login to the actitime application by calling login method..");		
+		result = CommonUtils.loginToActiTimeApplication();		
+		
+		// Checking the result to be equal to true
+		Assert.assertFalse(result, "Could not login to ActiTime application..");
+			
+		
+	}
+}
